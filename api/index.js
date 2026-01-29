25e2d9de0c205173c2397dba0b541ede586b3c89
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-29 14:59:08 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,15 +8,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-module.exports = app;
-// یہ لائن آپ کی سپلیش اسکرین اور لوگو کو لوڈ کروائے گی
-app.use(express.static(path.join(__dirname, "../public")));
-
-// جب کوئی مین سائٹ کھولے (Customer Page)
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
 // جب کوئی ایڈمن پیج کھولے
 app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/admin.html"));
