149cc20ebe5b3edd9233016f67ca42aade6cb7df
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-29 17:43:48 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// یہ لائن سب سے اہم ہے
+// سٹیٹک فائلز کے لیے راستہ
 app.use(express.static(path.join(__dirname, '../public')));
 
+// مین پیج کا راستہ
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// ٹیکسی پیج کا راستہ (یہی وہ حصہ ہے جو غائب تھا)
+app.get('/taxi', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/taxi.html'));
 });
 
 module.exports = app;
