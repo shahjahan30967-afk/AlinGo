8c5b45ba6fb8cdcabc870b2e8f40a8115792151a
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-30 06:24:05 +0500

    Update digital.routes.js

--- api/digital/digital.routes.js
+++ api/digital/digital.routes.js
@@ -1,2 +1,16 @@
 GET  /api/digital/services
 POST /api/digital/order
+const { getServices, orderService } = require("./digital.controller");
+
+module.exports = (req, res) => {
+  if (req.method === "GET" && req.url === "/api/digital/services") {
+    return getServices(req, res);
+  }
+
+  if (req.method === "POST" && req.url === "/api/digital/order") {
+    return orderService(req, res);
+  }
+
+  res.statusCode = 404;
+  res.end("Digital API Not Found");
+};
