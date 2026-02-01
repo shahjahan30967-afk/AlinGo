cae1dd6f2e585045a5ed58c4b0218fbc10e1e7f3
Aawan3967-oss <asifawan3967@gmail.com>
2026-02-02 00:20:28 +0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,10 @@ let deferredPrompt;
 window.addEventListener('beforeinstallprompt', e => {
   e.preventDefault();
   deferredPrompt = e;
-  document.getElementById('installBtn').style.display='block';
+  document.getElementById('installBtn').style.display = 'block';
 });
 
-document.getElementById('installBtn').onclick = async () => {
+document.getElementById('installBtn').onclick = () => {
   if(deferredPrompt){
     deferredPrompt.prompt();
     deferredPrompt = null;
@@ -16,15 +16,16 @@ document.getElementById('installBtn').onclick = async () => {
 setTimeout(()=>{
   document.getElementById('splash').style.display='none';
   document.querySelector('.topbar').classList.remove('hidden');
-  document.getElementById('content').classList.remove('hidden');
+  document.getElementById('main').classList.remove('hidden');
 },2500);
 
 function toggleMenu(){
-  const m=document.getElementById('sideMenu');
-  m.style.left=m.style.left==='0px'?'-260px':'0px';
+  const m=document.getElementById('menu');
+  m.style.left = m.style.left==='0px' ? '-240px' : '0px';
 }
 
 function showScreen(id){
   document.querySelectorAll('.screen').forEach(s=>s.classList.remove('active'));
   document.getElementById(id).classList.add('active');
+  document.getElementById('menu').style.left='-240px';
 }
