e1c3fabcc9762bb2c9486a88fba508855d97c50f
Aawan3967-oss <asifawan3967@gmail.com>
2026-02-02 08:57:16 +0500

    Delete routes/order.routes.js

--- routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import auth from "../middlewares/auth.middleware.js";
-import { acceptOrder } from "../controllers/order.controller.js";
-
-const router = express.Router();
-
-router.post("/", auth, acceptOrder);
-
-export default router;
