19ce99496b90e444bb5f7a657ba45c6c823cac8b
Aawan3967-oss <asifawan3967@gmail.com>
2026-02-02 13:43:32 +0500

    Update ride.routes.js

--- routes/ride.routes.js
+++ routes/ride.routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const rideController = require("../controllers/ride.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
+const Ride = require("../models/Ride");
+const auth = require("../middlewares/auth.middleware");
 
-// OTP protected routes
-router.post("/create", authMiddleware, rideController.createRide);
-router.get("/my-rides", authMiddleware, rideController.getRides);
-router.get("/:id", authMiddleware, rideController.getRide);
+router.post("/create", auth, async (req, res) => {
+  const ride = await Ride.create({
+    user: req.user.id,
+    pickup: req.body.pickup,
+    drop: req.body.drop
+  });
+  res.json(ride);
+});
+
+router.get("/my", auth, async (req, res) => {
+  const rides = await Ride.find({ user: req.user.id });
+  res.json(rides);
+});
 
 module.exports = router;
