8cfc3b167f2db260f0793dc49a4d3633716555c1
Aawan3967-oss <asifawan3967@gmail.com>
2026-02-02 13:42:14 +0500

    Update otp.routes.js

--- routes/otp.routes.js
+++ routes/otp.routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const otpController = require("../controllers/otp.controller");
+const admin = require("../config/firebase");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-router.post("/send", otpController.sendOtp);
-router.post("/verify", otpController.verifyOtp);
+router.post("/verify", async (req, res) => {
+  const decoded = await admin.auth().verifyIdToken(req.body.token);
+
+  let user = await User.findOne({ phone: decoded.phone_number });
+  if (!user) user = await User.create({ phone: decoded.phone_number });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token });
+});
 
 module.exports = router;
