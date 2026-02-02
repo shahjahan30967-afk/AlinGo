f6728c25657e231a856cc72c51b734847f547b0d
Aawan3967-oss <asifawan3967@gmail.com>
2026-02-02 13:40:53 +0500

    Update auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,13 +1,14 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
-export default function auth(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "No Token" });
+module.exports = (req, res, next) => {
+  const auth = req.headers.authorization;
+  if (!auth) return res.status(401).json({ message: "No token" });
 
   try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(auth.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
     next();
   } catch {
-    res.status(401).json({ message: "Invalid Token" });
+    res.status(401).json({ message: "Invalid token" });
   }
-}
+};
