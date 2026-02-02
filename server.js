c161527878cfc017c7d20e33e4f330886787ad8d
Aawan3967-oss <asifawan3967@gmail.com>
2026-02-02 10:11:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use("/api/otp", require("./routes/otp.routes"));
 // Ride routes
 app.use("/api/ride", require("./routes/ride.routes"));
 
-// Logistics routes (OTP Protected)
+// Logistics routes
 app.use("/api/logistics", require("./routes/logistics.routes"));
+
+// Food routes
+app.use("/api/food", require("./routes/food.routes"));
 
 module.exports = app;
